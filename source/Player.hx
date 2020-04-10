acdd9abac2e41f8f28a4cb3edd2e599424f8d069
tom.finney@cd2solutions.co.uk <tom.finney@cd2solutions.co.uk>
2020-04-10 17:28:17 +0100

    adds a dope sprite

--- source/Player.hx
+++ source/Player.hx
@@ -1,22 +1,24 @@
 package;
 
+import flixel.FlxObject;
 import flixel.math.FlxPoint;
-import flixel.util.FlxColor;
 import flixel.FlxSprite;
 import flixel.FlxG;
 
 class Player extends FlxSprite {
 	static inline var SPEED:Float = 200;
 
-	var up:Bool = false;
-	var down:Bool = false;
-	var left:Bool = false;
-	var right:Bool = false;
-
 	public function new(x:Float = 0, y:Float = 0) {
 		super(x, y);
 
-		makeGraphic(16, 16, FlxColor.BLUE);
+		loadGraphic(AssetPaths.player__png, true, 16, 16);
+
+		setFacingFlip(FlxObject.LEFT, false, false);
+		setFacingFlip(FlxObject.RIGHT, true, false);
+
+		animation.add("lr", [3, 4, 3, 5], 6, false);
+		animation.add("u", [6, 7, 6, 8], 6, false);
+		animation.add("d", [0, 1, 0, 2], 6, false);
 
 		drag.x = 1600;
 		drag.y = 1600;
@@ -29,10 +31,10 @@ class Player extends FlxSprite {
 	}
 
 	function updateMovement() {
-		up = FlxG.keys.anyPressed([UP, W]);
-		down = FlxG.keys.anyPressed([DOWN, S]);
-		left = FlxG.keys.anyPressed([LEFT, A]);
-		right = FlxG.keys.anyPressed([RIGHT, D]);
+		var up = FlxG.keys.anyPressed([UP, W]);
+		var down = FlxG.keys.anyPressed([DOWN, S]);
+		var left = FlxG.keys.anyPressed([LEFT, A]);
+		var right = FlxG.keys.anyPressed([RIGHT, D]);
 
 		if (up && down) {
 			up = false;
@@ -48,27 +50,48 @@ class Player extends FlxSprite {
 			var newAngle:Float = 0;
 
 			if (up) {
+				facing = FlxObject.UP;
+
 				newAngle = -90;
+
 				if (left) {
 					newAngle -= 45;
 				} else if (right) {
 					newAngle += 45;
 				}
 			} else if (down) {
+				facing = FlxObject.DOWN;
+
 				newAngle = 90;
+
 				if (left) {
 					newAngle += 45;
 				} else if (right) {
 					newAngle -= 45;
 				}
 			} else if (left) {
+				facing = FlxObject.LEFT;
+
 				newAngle = 180;
 			} else if (right) {
+				facing = FlxObject.RIGHT;
+
 				newAngle = 0;
 			}
 
 			velocity.set(SPEED, 0);
 			velocity.rotate(FlxPoint.weak(0, 0), newAngle);
+		}
+
+		if ((velocity.x != 0 || velocity.y != 0) && touching == FlxObject.NONE) {
+			switch (facing) {
+				case FlxObject.LEFT, FlxObject.RIGHT:
+					animation.play("lr");
+				case FlxObject.UP:
+					animation.play("u");
+				case FlxObject.DOWN:
+					animation.play("d");
+			}
 		}
 	}
 }
