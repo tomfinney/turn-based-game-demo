77ee802bc91e7ccfba6dc2c8597ce41c64661bc5
tom.finney@cd2solutions.co.uk <tom.finney@cd2solutions.co.uk>
2020-04-10 16:31:11 +0100

    adds some basic key controls

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -3,7 +3,12 @@ package;
 import flixel.FlxState;
 
 class PlayState extends FlxState {
+	var player:Player;
+
 	override public function create() {
+		player = new Player(20, 20);
+		add(player);
+
 		super.create();
 	}
 
